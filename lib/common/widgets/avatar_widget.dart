ab64f91c6db45fa557c01fb49824508b2ec20092
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2025-03-23 15:27:23 +0400

    avatar url

--- lib/common/widgets/avatar_widget.dart
+++ lib/common/widgets/avatar_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:heal_v/shared/feature/auth/auth_bloc.dart';
+import 'package:heal_v/theme/ext/extension.dart';
 
 class AvatarWidget extends StatelessWidget {
   final double radius;
@@ -8,26 +11,31 @@ class AvatarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        CircleAvatar(
-          radius: radius,
-          backgroundImage: const NetworkImage(
-            'https://example.com/profile.jpg',
-          ),
-        ),
-        if (isEditable)
-          const Positioned(
-            bottom: 0,
-            right: 0,
-            child: CircleAvatar(
-              radius: 16,
-              backgroundColor: Color(0xFFE582DE),
-              child: Icon(Icons.edit, color: Colors.white, size: 16),
+    return BlocSelector<AuthBloc, AuthBlocState, String?>(
+      selector: (AuthBlocState state) => state.user?.photoURL,
+      builder: (BuildContext context, String? url) {
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            CircleAvatar(
+              radius: radius,
+              backgroundColor: context.primary,
+              backgroundImage: url != null ? NetworkImage(url) : null,
+              child: url == null ? Icon(Icons.person, size: radius) : null,
             ),
-          ),
-      ],
+            if (isEditable)
+              const Positioned(
+                bottom: 0,
+                right: 0,
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundColor: Color(0xFFE582DE),
+                  child: Icon(Icons.edit, color: Colors.white, size: 16),
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
